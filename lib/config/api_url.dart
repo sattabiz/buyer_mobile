798ba175cbc1b90595068f01c566fb112acfc331
Atakan <atakangobelek@satta.biz>
2023-09-26 10:48:54 +0300

    configuration proposal view variable from service

--- lib/config/api_url.dart
+++ lib/config/api_url.dart
@@ -2,7 +2,7 @@ class ApiUrls{
   static const String baseUrl = "https://test.satta.biz/api/v1";
   static const String login = "$baseUrl/login.json";
   static const String logout = "$baseUrl/logout.json";
-  static const String proposals = "$baseUrl/supplier_proposals_by_state.json?proposal_state='proposal_stvs','ordered'&customer_company_id=91";
+  static const String proposals = "$baseUrl/supplier_proposals_by_state.json?proposal_state='pending','replied'&customer_company_id=91";
   static const String orders = "$baseUrl/supplier_order_list_by_state.json?state='order_approved','order_pending','order_confirmed'&customer_id=91";
   static const String confirmOrder = "$baseUrl/supplier_confirm_order.json";
   static const String invoices = "$baseUrl/supplier_invoice_list_by_state.json?state='invoice_approved','invoice_pending'&customer_company_id=91";
