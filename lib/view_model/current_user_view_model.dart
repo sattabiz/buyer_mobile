86753d2e12235cc0567c9439ef39285c3af84947
Atakan <atakangobelek@satta.biz>
2023-10-11 00:25:21 +0300

    added user info service and configuration chatbox

---
+++ lib/view_model/current_user_view_model.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../config/api_url.dart';
+import '../model/get_current_user_info_model.dart';
+import '../service/get_services.dart';
+
+final getCurrentUserInfoProvider = FutureProvider.autoDispose<CurrentUserInfoModel>((ref) async {
+  final apiService = ApiService();
+  Response response;
+  try {
+    response = await apiService.get(url: ApiUrls.userInfo);
+  } catch (e) {
+    if (e is DioException) {
+      if (e.response?.statusCode != 200) {
+      }
+    }
+    rethrow;
+  }
+  debugPrint(response.toString());
+  CurrentUserInfoModel currentUserInfoModel =
+          CurrentUserInfoModel.fromMap(response.data);
+
+  ref.read(userIdProvider.notifier).state = currentUserInfoModel;
+
+  return currentUserInfoModel;
+});
+
+final userIdProvider = StateProvider<CurrentUserInfoModel>((ref) => CurrentUserInfoModel());
